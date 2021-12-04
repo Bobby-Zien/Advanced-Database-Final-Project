6fc0bbad9cf5c10f6fbd6b6666c372ae323b1763
aubrey <aaaaubreys@gmail.com>
2021-12-04 16:09:57 -0500

    add parser

---
+++ code/iohandler.py
@@ -0,0 +1,22 @@
+import re
+class Parser:
+
+    def __init__(self, file_name) -> None:
+        self.FINISH_FLAG = False
+        self.file_name = file_name
+    
+    def parse_file(self):
+        try:
+            with open(self.file_name, 'r') as f:
+                lines = f.readlines()
+                for line in lines:
+                    args = self._parse_line(line)
+                    print(args)
+        except IOError:
+            print("FATAL ERROR: Invalid input file {}".format(self.file_name))
+
+    def _parse_line(self, line:str):
+        line = line.split('#')[0].strip()
+        return re.findall(r"[\w]+", line)
+
+
